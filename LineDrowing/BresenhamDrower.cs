b5e87e6165d30f30a02ed0f2d9e1faaa1f0cd9eb
Anas Alamir <anasalamir06@gmail.com>
2025-04-10 18:14:47 +0200

    split main code

---
+++ LineDrowing/BresenhamDrower.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineDrowing
+{
+    internal class BresenhamDrower
+    {
+        public static void DrowBresenham()
+        {
+            int value = 10;
+            Point point1 = new Point(10 + value, 3 + value);
+            Point point2 = new Point(10 + value, 40 + value);
+            Point point3 = new Point(20 + value, 20 + value);
+            Point point4 = new Point(100 + value, 0 + value);
+            Point point5 = new Point(40 + value, 50 + value);
+
+            Bresenham.DrowLine(point1, point2);
+            Bresenham.DrowLine(point1, point4);
+            Bresenham.DrowLine(point1, point3);
+            Bresenham.DrowLine(point2, point3);
+            Bresenham.DrowLine(point2, point5);
+            Bresenham.DrowLine(point3, point4);
+            Bresenham.DrowLine(point3, point5);
+            Bresenham.DrowLine(point4, point5);
+
+            Utility.PutPointWithSymbol(point1, "p1");
+            Utility.PutPointWithSymbol(point2, "p2");
+            Utility.PutPointWithSymbol(point3, "p3");
+            Utility.PutPointWithSymbol(point4, "p4");
+            Utility.PutPointWithSymbol(point5, "p5");
+        }
+    }
+}
---
+++ LineDrowing/DDADrower.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineDrowing
+{
+    internal class DDADrower
+    {
+        public static void DrowDDA()
+        {
+            Point point1 = new Point(10, 3);
+            Point point2 = new Point(10, 40);
+            Point point3 = new Point(20, 20);
+            Point point4 = new Point(100, 0);
+            Point point5 = new Point(40, 50);
+
+            DDA.DrowLine(point1, point2);
+            DDA.DrowLine(point1, point4);
+            DDA.DrowLine(point1, point3);
+            DDA.DrowLine(point2, point3);
+            DDA.DrowLine(point2, point5);
+            DDA.DrowLine(point3, point4);
+            DDA.DrowLine(point3, point5);
+            DDA.DrowLine(point4, point5);
+
+            Utility.PutPointWithSymbol(point1, "p1");
+            Utility.PutPointWithSymbol(point2, "p2");
+            Utility.PutPointWithSymbol(point3, "p3");
+            Utility.PutPointWithSymbol(point4, "p4");
+            Utility.PutPointWithSymbol(point5, "p5");
+        }
+    }
+}
